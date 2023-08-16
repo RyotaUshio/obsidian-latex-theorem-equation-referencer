40d374ac58006782453492c4cc40a41540b36a61
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2023-08-17 03:13:25 +0900

    move css inside make_styles.py to a separate file styles/styles_main.css

--- make_styles.py
+++ make_styles.py
@@ -1,14 +1,24 @@
-# Generate styles.css
+"""Generate styles.css from styles/*.css.
+
+Each of CSS files other than styles_main.css defines a preset style, and 
+styles_main.css defines other styles needed for this plugin.
+"""
 
 from pathlib import Path
-import textwrap
 
 if __name__ == "__main__":
     source = Path("./styles")
+    main = Path("./styles/styles_main.css")
     target = Path("./styles.css")
+
+    assert main.exists()
 
     lines = []
     for source_style in source.glob("*.css"):
+        if source_style == main:
+            continue
+
+        # preset styles
         with open(source_style) as f:
             for line in f:
                 if line.lstrip().startswith('.math-callout'):
@@ -17,71 +27,14 @@ if __name__ == "__main__":
                     else:
                         line = f'.math-callout-{source_style.stem}' + line
                 lines.append(line)
+        lines.append("\n")
+
+    with open(main) as f:
+        main_content = f.read()
 
     with open(target, 'w') as f:
         f.write(
-            textwrap.dedent("""
-            /* This file is auto-generated. Do not edit it. */
-                            
-            .math-booster-preview {
-                cursor: text;
-            }
-                            
-            .cm-embed-block {
-                position: relative;
-            }
-
-            .cm-embed-block .math-booster-preview-edit-button {
-                padding: var(--size-2-2) var(--size-2-3);
-                position: absolute;
-                right: var(--size-2-2);
-                top: var(--size-2-2);
-                opacity: 0;
-                display: flex;
-                border-radius: var(--radius-s);
-            }           
-                            
-            .cm-embed-block:hover .math-booster-preview-edit-button {
-                transition: 0s;
-                opacity: 1
-            }
-                            
-            /* Unlike in callouts, Obsidian natively renders MathJax in blockquotes. But it is rather buggy 
-            (see https://forum.obsidian.md/t/live-preview-support-math-block-in-quotes/32564/2), 
-            so we need to replace it with this plugin's editor extension. */
-            .cm-line .math.math-block.cm-embed-block:has(> mjx-container.MathJax:not(.math-booster-preview) > mjx-math[display="true"]) {
-                display: none;
-            }
-                            
-            .math-callout {
-                position: relative;
-            }
-                            
-            .math-callout-setting-button {
-                padding-bottom: var(--size-2-2);
-                padding-right: var(--size-2-3);
-                position: absolute;
-                right: var(--size-2-2);
-                bottom: var(--size-2-2);
-                opacity: 0;
-            }
-            
-            .math-callout:hover .math-callout-setting-button {
-                transition: 0s;
-                opacity: 1
-            }
-
-            """)
+            "/* This file is auto-generated. Do not edit it. */\n\n"
+            + main_content + "\n"
             + ''.join(lines)
-            + textwrap.dedent(
-            """
-            .math-callout-font-family-inherit {
-                font-family: inherit !important;
-            }
-
-            .math-booster-title-form,
-            .math-booster-label-form {
-                width: 300px;
-            }
-            """)
         )
