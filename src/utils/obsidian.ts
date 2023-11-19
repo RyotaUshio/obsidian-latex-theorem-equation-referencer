9c35e3bd1154487ee7c3c702fb62aa3d86346e7b
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2023-11-19 16:16:52 +0900

    minor change

--- src/utils/obsidian.ts
+++ src/utils/obsidian.ts
@@ -163,14 +163,15 @@ export async function openFileAndSelectPosition(app: App, file: TFile, position:
             // previewMode.applyScroll(position.start.line) doesn't center-align the target block, 
             // so we have to do it manually.
             const previewMode = (leaf.view as MarkdownView).previewMode;
-            const renderer = (previewMode as any).renderer as MarkdownPostProcessorContext & { sizerEl: HTMLElement };
+            const renderer = (previewMode as any).renderer as MarkdownPostProcessorContext & { sizerEl: HTMLElement, highlightEl: (el: HTMLElement) => void  };
+            
             // find the div corresponding the target block
             const div = Array.from(renderer.sizerEl.querySelectorAll<HTMLElement>(':scope > div'))
                 .find((div) => renderer.getSectionInfo(div)?.lineStart === position.start.line);
+            if (!div) return;
 
-            // @ts-ignore
             renderer.highlightEl(div);
-            div?.scrollIntoView({ block: 'center' });
+            div.scrollIntoView({ block: 'center' });
         }, 50);
     }
 }
