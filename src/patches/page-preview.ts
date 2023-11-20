c17bfc71927b8474f5c5793a55fecc51e52c4b72
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2023-11-21 01:05:24 +0900

    update an outdated comment

--- src/patches/page-preview.ts
+++ src/patches/page-preview.ts
@@ -4,7 +4,7 @@ import MathBooster from '../main';
 
 // Inspired by Hover Editor (https://github.com/nothingislost/obsidian-hover-editor/blob/c038424acb15c542f0ad5f901d74c75d4316f553/src/main.ts#L396)
 
-// Add "src" attribute to hover page preview elements so that we can get linktext to display theorem/equation numbers in it
+// Save the last linktext that triggered hover page preview in the plugin instance to display theorem/equation numbers in it
 
 export const patchPagePreview = (plugin: MathBooster) => {
     const { app } = plugin;
