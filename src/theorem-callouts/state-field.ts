efa3834fe318ddc6af4b1f8f31516fb21b07658a
Artemy Efimov <artemyfmv@gmail.com>
2024-02-10 00:40:29 +0500

    Separate numbering for each callout type

--- src/theorem-callouts/state-field.ts
+++ src/theorem-callouts/state-field.ts
@@ -5,14 +5,23 @@ import LatexReferencer from 'main';
 import { readTheoremCalloutSettings } from 'utils/parse';
 import { editorInfoField } from 'obsidian';
 
+import { THEOREM_LIKE_ENV_IDs, TheoremLikeEnvID } from 'env';
+
 export const CALLOUT = /HyperMD-callout_HyperMD-quote_HyperMD-quote-([1-9][0-9]*)/;
 
 export class TheoremCalloutInfo extends RangeValue {
-    constructor(public index: number | null) {
+    constructor(public index: number | null, public type: string) {
         super();
     }
 }
 
+function createEmptyCalloutInits(): Record<string, number> {
+    let inits = {} as Record<string, number>;
+    THEOREM_LIKE_ENV_IDs.forEach((id, _) => {
+        inits[id] = 0
+    });
+    return inits;
+}
 
 export const createTheoremCalloutsField = (plugin: LatexReferencer) => StateField.define<RangeSet<TheoremCalloutInfo>>({
     create(state: EditorState) {
@@ -20,7 +29,7 @@ export const createTheoremCalloutsField = (plugin: LatexReferencer) => StateFiel
         // do not number theorems in canvas to make live preview consistent with reading view
         if (!state.field(editorInfoField).file) return RangeSet.empty;
 
-        const ranges = getTheoremCalloutInfos(plugin, state, state.doc, 0, 0);
+        const ranges = getTheoremCalloutInfos(plugin, state, state.doc, 0, createEmptyCalloutInits());
         return RangeSet.of(ranges);
     },
     update(value: RangeSet<TheoremCalloutInfo>, tr: Transaction) {
@@ -47,12 +56,12 @@ export const createTheoremCalloutsField = (plugin: LatexReferencer) => StateFiel
 
         value = value.map(changeDesc);
 
-        let init = 0;
+        let inits = createEmptyCalloutInits();
         value.between(0, minChangedPosition, (from, to, info) => {
-            if (to < minChangedPosition && info.index !== null) init = info.index + 1;
+            if (to < minChangedPosition && info.index !== null) inits[info.type] = info.index + 1;
         });
 
-        const updatedRanges = getTheoremCalloutInfos(plugin, tr.state, tr.newDoc, minChangedPosition, init);
+        const updatedRanges = getTheoremCalloutInfos(plugin, tr.state, tr.newDoc, minChangedPosition, inits);
         return value.update({
             add: updatedRanges,
             filter: () => false,
@@ -62,12 +71,12 @@ export const createTheoremCalloutsField = (plugin: LatexReferencer) => StateFiel
 });
 
 
-function getTheoremCalloutInfos(plugin: LatexReferencer, state: EditorState, doc: Text, from: number, init: number): Range<TheoremCalloutInfo>[] {
+function getTheoremCalloutInfos(plugin: LatexReferencer, state: EditorState, doc: Text, from: number, inits: Record<string, number>): Range<TheoremCalloutInfo>[] {
     const ranges: Range<TheoremCalloutInfo>[] = [];
     // syntaxTree returns a potentially imcomplete tree (limited by viewport), so we need to ensure it's complete
     const tree = ensureSyntaxTree(state, doc.length) ?? syntaxTree(state);
 
-    let theoremIndex = init; // incremented when a auto-numbered theorem is found
+    let theoremIndexes = inits; // incremented when a auto-numbered theorem is found
 
     tree.iterate({
         from, to: doc.length,
@@ -84,7 +93,7 @@ function getTheoremCalloutInfos(plugin: LatexReferencer, state: EditorState, doc
             const settings = readTheoremCalloutSettings(text, plugin.extraSettings.excludeExampleCallout);
             if (!settings) return false;
 
-            const value = new TheoremCalloutInfo(settings.number === 'auto' ? theoremIndex++ : null);
+            const value = new TheoremCalloutInfo(settings.number === 'auto' ? theoremIndexes[settings.type]++ : null, settings.type);
             const range = value.range(node.from, node.to);
             ranges.push(range);
 
